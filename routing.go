0d094fa3ecf206e9dcc5d9dec7e8393c94dbee3d
mfauzanz <bukanelu@gmail.com>
2024-08-13 21:45:36 +0700

    routing--done

--- routing.go
+++ routing.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	handlerIndex := func(w http.ResponseWriter, r *http.Request) {
@@ -9,4 +12,11 @@ func main() {
 
 	http.HandleFunc("/", handlerIndex)
 	http.HandleFunc("/index", handlerIndex)
+
+	http.HandleFunc("/data", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello again"))
+	})
+
+	fmt.Println("server started at localhost:9000")
+	http.ListenAndServe(":9000", nil)
 }
